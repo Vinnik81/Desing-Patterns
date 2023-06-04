48baaa25003d68b5212a8ab005dc651b7c762e58
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-06-04 18:17:24 +0600

    Facade Pattern & Prototype & Builder

---
+++ Builder/Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Person person = new Person().SetSurname("Vinnik").SetName("Vladimir").SetPhone("89609936545").SetId(1);
+
+Console.WriteLine(person.ToString());
+
+class Person
+{
+    private int id;
+    private string name;
+    private string surname;
+    private string phone;
+
+    public Person SetId(int id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    public int GetId()
+    {
+        return id;
+    }
+
+    public Person SetName(string name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Person SetSurname(string surname)
+    {
+        this.surname = surname;
+        return this;
+    }
+
+    public string GetSurname()
+    {
+        return surname;
+    }
+
+    public Person SetPhone(string phone)
+    {
+        this.phone = phone;
+        return this;
+    }
+
+    public string GetPhone()
+    {
+        return phone;
+    }
+
+    public override string? ToString()
+    {
+        return this.name + " " + this.surname + " " + this.phone;
+    }
+}
+
+
+//Person person = new Person();
+//person.SetSurname("Vinnik");
+//person.SetName("Vladimir");
+//person.SetPhone("89609936545");
+//person.SetId(1);
+
+//Console.WriteLine(person.ToString());
+
+//class Person
+//{
+//    private int id;
+//    private string name;
+//    private string surname;
+//    private string phone;
+
+//    public void SetId(int id)
+//    {
+//        this.id = id;
+//    }
+
+//    public int GetId()
+//    {
+//        return id;
+//    }
+
+//    public void SetName(string name) 
+//    {
+//        this.name = name;
+//    }
+
+//    public string GetName() 
+//    {
+//        return name;
+//    }
+
+//    public void SetSurname(string surname)
+//    {
+//        this.surname = surname;
+//    }
+
+//    public string GetSurname()
+//    {
+//        return surname;
+//    }
+
+//    public void SetPhone(string phone)
+//    { 
+//        this.phone = phone;
+//    }
+
+//    public string GetPhone()
+//    {
+//        return phone;
+//    }
+
+//    public override string? ToString()
+//    {
+//        return this.name + " " + this.surname + " " + this.phone;
+//    }
+//}
---
+++ Prototype/Program.cs
@@ -0,0 +1,110 @@
+﻿Circle circle = new Circle();
+circle.radius = 1;
+Console.WriteLine(circle.radius);
+for (int i = 0; i < 1000; i++)
+{
+    Console.WriteLine((circle.Clone() as Circle).radius);
+}
+
+class Circle : ICloneable
+{
+    public int radius { get; set; }
+
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+}
+
+//Circle circle = new Circle();
+//circle.y = 10;
+//circle.x = 20;
+//circle.radius = 1;
+
+//Console.WriteLine(circle);
+//Console.WriteLine(circle.Clone());
+
+
+
+//for (int i = 0; i < 1000; i++)
+//{
+//    Console.WriteLine(circle.Clone());
+//}
+
+
+//abstract class Shape
+//{
+//    public int x { get; set; }
+//    public int y { get; set; }
+//    public string color { get; set; }
+//    public abstract Shape Clone();
+
+//    public Shape()
+//    {
+
+//    }
+
+//    public Shape(Shape shape)
+//    {
+//        if (shape != null)
+//        {
+//            this.x = shape.x;
+//            this.y = shape.y;
+//            this.color = shape.color;
+//        }
+//    }
+//}
+
+//class Circle: Shape
+//{
+//    public int radius { get; set; }
+
+//    public Circle()
+//    {
+
+//    }
+
+//    public Circle(Circle circle): base(circle)
+//    {
+//        if (circle != null)
+//        {
+//            this.radius = circle.radius;
+//        }
+//    }
+
+//    public override Shape Clone()
+//    {
+//        return new Circle(this);
+//    }
+
+//    public override string? ToString()
+//    {
+//        return this.y + " " + this.x + " " +this.radius;
+//    }
+//}
+
+//class Rectangle : Shape
+//{
+//    public int width { get; set; }
+//    public int height { get; set; }
+
+//    public Rectangle()
+//    {
+
+//    }
+
+
+//    public Rectangle(Rectangle rectangle) : base(rectangle)
+//    {
+//        if (rectangle != null)
+//        {
+//            this.height = rectangle.height;
+//            this.width = rectangle.width;
+//        }
+//    }
+
+//    public override Shape Clone()
+//    {
+//        return new Rectangle(this);
+//    }
+//}
