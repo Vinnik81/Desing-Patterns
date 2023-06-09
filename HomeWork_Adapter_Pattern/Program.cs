f87b6b1b2dd2f998576a1fe94c926fbea2e99f17
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-06-09 19:30:56 +0600

    HomeWork_Adapter DONE

--- HomeWork_Adapter_Pattern/Program.cs
+++ HomeWork_Adapter_Pattern/Program.cs
@@ -1,15 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HomeWork_Adapter_Pattern;
 
-var url = new List<IApiUrl> 
-{ 
-    new OmbpApiAdapter(new OmbApi()),
-    new OpenWeatherAdapter(new OpenWeather()),
-    new DeezerAdapter(new Dezer())
-};
+//#region Variant 1
+//var url = new List<IApiUrl>
+//{
+//    new OmbpApiAdapter(new OmbApi()),
+//    new OpenWeatherAdapter(new OpenWeather()),
+//    new DeezerAdapter(new Dezer())
+//};
 
 
-foreach (var item in url)
+//foreach (var item in url)
+//{
+//    item.Url();
+//}
+//#endregion
+
+#region Variant 2
+Console.WriteLine("Введите название сайта: OmbpApi, OpenWeather, Deezer");
+string name = Console.ReadLine();
+
+switch (name)
 {
-    item.Url();
+    case "OmbpApi":
+        var urlOmbp = new OmbpApiAdapter(new OmbApi());
+        urlOmbp.Url();
+        break;
+    case "OpenWeather":
+        var urlOpenWeather = new OpenWeatherAdapter(new OpenWeather());
+        urlOpenWeather.Url();
+        break;
+    case "Deezer":
+        var urlDeezer = new DeezerAdapter(new Dezer());
+        urlDeezer.Url();
+        break;
+    default:
+        Console.WriteLine("Такого сайта нет в списке");
+        break;
 }
+#endregion
