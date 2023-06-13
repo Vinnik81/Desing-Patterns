8c4a96a0cac90386a6deceeecfef7a16cb0f6f5d
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-06-13 20:05:53 +0600

    HomeWork_Iterator DONE

---
+++ HomeWork_Iterator/IteratorAggregate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace HomeWork_Iterator
+{
+    abstract class IteratorAggregate : IEnumerable
+    {
+        
+        public abstract IEnumerator GetEnumerator();
+    }
+}
---
+++ HomeWork_Iterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_Iterator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var collection = new WordsCollection();
+            collection.AddItem("Первый");
+            collection.AddItem("Второй");
+            collection.AddItem("Третий");
+
+            Console.WriteLine("Прямой обход:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nОбратный обход:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
